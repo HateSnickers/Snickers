790373f08643cd3043773139bd0118cfbe34e2a2
HateSnickers <cervenytadeas@gmail.com>
2023-10-02 17:53:49 +0200

    sd

--- kalkulacka/Calculator/Program.cs
+++ kalkulacka/Calculator/Program.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-/*
- * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
- * Extended by students.
- */
 
 namespace Calculator
 {
@@ -15,65 +6,77 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            string op = "";
             string Text;
-            Console.WriteLine("zadej cislo1");
+            double result = 0;
+            Console.WriteLine("Vyberte režim: (1) Desítková, (2) Dvojková, (3) Šestnáctková");
+            Text = Console.ReadLine();
+
+            int baseChoice = 10;
+
+            if (Text == "2")
+            {
+                baseChoice = 2; 
+            }
+            else if (Text == "3")
+            {
+                baseChoice = 16; 
+            }
+
+            Console.WriteLine("Zadejte první číslo");
             Text = Console.ReadLine();
             double cislo = Convert.ToDouble(Text);
 
-
-            Console.WriteLine("zadej cislo2"); 
+            Console.WriteLine("Zadejte druhé číslo");
             Text = Console.ReadLine();
             double cislo2 = Convert.ToDouble(Text);
 
+            Console.WriteLine("Zadejte operaci");
+            Text = Console.ReadLine();
+            op = Convert.ToString(Text);
 
-            Console.WriteLine("zadej operaci"); 
-            Text = Console.ReadLine();          
-            double result = 0;
- 
-            
-            if (Text == "+") {
-                result = cislo + cislo2;
+            switch (op)
+            {
+                case "+":
+                    result = cislo + cislo2;
+                    break;
+                case "-":
+                    result = cislo - cislo2;
+                    break;
+                case "*":
+                    result = cislo * cislo2;
+                    break;
+                case "/":
+                    result = cislo / cislo2;
+                    break;
+                case "mocnina":
+                    result = Math.Pow(cislo, cislo2);
+                    break;
+                case "odmocnina":
+                    result = Math.Sqrt(cislo);
+                    break;
+                case "log":
+                    result = Math.Log(cislo);
+                    break;
+                default:
+                    Console.WriteLine("Neplatná operace");
+                    break;
             }
-            else if (Text == "-") {
-                result = cislo - cislo2;
+
+            if (baseChoice == 2)
+            {
+                Console.WriteLine("Výsledek v dvojkové soustavě: " + Convert.ToString((int)result, 2));
             }
-            else if (Text == "*"){
-                result = cislo * cislo2;
-            }
-            else if(Text == "/"){
-                result = cislo / cislo2;
+            else if (baseChoice == 16)
+            {
+                Console.WriteLine("Výsledek v šestnáctkové soustavě: " + Convert.ToString((int)result, 16));
             }
             else
-                Console.WriteLine("spatny znaminko Top G");
-            Console.WriteLine("Vysledek"); 
-            Console.WriteLine(result);
-            
-           
-            /*
-             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
-             * 
-             * ZADANI
-             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
-             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
-             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
-             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
-             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
-             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
-             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
-             * 7) Vypis promennou result do konzole
-             * 
-             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
-             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
-             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
-             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
-             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
-             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
-             */
+            {
+                Console.WriteLine("Výsledek v desítkové soustavě: " + result);
+            }
 
-            //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
-
-            Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
+            Console.ReadKey();
         }
     }
 }
